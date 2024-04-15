6cf339d19ac1614fc39f868522d30d960ede8fbf
Roman-Maliarenko <romamalarenko1@gmail.com>
2024-04-15 23:42:20 +0300

    git

--- index.js
+++ index.js
@@ -1,31 +1,35 @@
 // Задание 1
+//обычная функцая
+function number(array, index){
+    return array [index]
+
+}
 
 const getElement = (array, index) => {
     return array[index];
 }
-const arr = ["Mondey", "Tuesday", "Wednesday", "Thursday", "Fridy"];
+const arr = ["Mondey", "Tuesday", "Wednesday", "Thursday", "Friday"];
 const index = 3;
 console.log(getElement(arr, index));
 
 // Задание 2
-
 const thisWeather = (weather, decode) => {
 const description = decode(weather);
 return `Today is : ${description}`;
 }
-const codeWeather = (code) => {
-switch (code){
-    case 1:
-        return `SQ`;
-    case 2:
-        return `PO`;
-    case 3:
-        return `FC`;
-    case 4:
-        return `BR`;
-    case 5:
-        return `Nothing`    
+const decode = (weather) => {
+switch (weather){
+    case "SQ":
+        return `шквал`;
+    case "PO":
+        return `пыльный вихрь`;
+    case "FC":
+        return `торнадо`;
+    case "BR":
+        return `дымка (видимость от 1 до 9 км)`;
+    case "HZ":
+        return `мгла (видимость менее 10 км)`; 
     }
 };
-const weather = 1;
-console.log(thisWeather(weather, codeWeather));
+const weather = "SQ";
+console.log(thisWeather(weather, decode));
